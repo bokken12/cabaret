92b62a66faf6e8d5e23b533e9a24ca2795cd4aa0
Joel M <stopwatchj@gmail.com>
2026-05-25 09:02:05 -0700

    add TODOs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
+// TODO-someday(joel for crouton): why does this spec differ from tsconfig.test.json?
 export default defineConfig({
   test: {
     include: ['packages/*/src/**/*.{test,spec}.ts'],
