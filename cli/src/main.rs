5b535da89fdd6a77949d27b050180d93c2632ed4
Joel <me@joel.place>
2026-08-08 18:18:42 -0700

    init cli

---
+++ cli/src/main.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Command {
+    Diff,
+    Mark,
+    Owners,
+    Parents,
+}
+
+#[derive(Parser)]
+#[command(name = "cab", version, about = "Cabaret Code Review")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Diff => todo!()
+        , Command::Mark => todo!()
+        Command::Owners => todo!(),
+        Command::Parents => todo!()
+    }
+}
