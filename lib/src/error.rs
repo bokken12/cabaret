7cb5a199cd48ab01605945684871c5ad231163a2
Joel <me@joel.place>
2026-08-14 19:00:24 -0700

    napi errors

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -11,3 +11,8 @@ impl<E: Display> From<E> for Error {
 impl fmt::Debug for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { f.write_str(&self.0) }
 }
+
+#[cfg(feature = "napi")]
+impl From<Error> for napi::Error {
+    fn from(error: Error) -> Self { Self::from_reason(error.0) }
+}
--- node/src/lib.rs
+++ node/src/lib.rs
@@ -4,18 +4,12 @@
 use cabaret_lib::Cabaret;
 use napi_derive::napi;
 
-fn napi_error(error: &cabaret_lib::Error) -> napi::Error { napi::Error::from_reason(format!("{error:?}")) }
-
-fn open(dir: &str) -> napi::Result<Cabaret> { Cabaret::open(dir).map_err(|error| napi_error(&error)) }
-
 #[napi]
 pub fn changes(dir: String) -> napi::Result<Vec<String>> {
-    let changes = open(&dir)?.changes().map_err(|error| napi_error(&error))?;
-    Ok(changes.iter().map(ToString::to_string).collect())
+    Ok(Cabaret::open(&dir)?.changes()?.iter().map(ToString::to_string).collect())
 }
 
 #[napi]
 pub fn current_change(dir: String) -> napi::Result<String> {
-    let change = open(&dir)?.current_change().map_err(|error| napi_error(&error))?;
-    Ok(change.to_string())
+    Ok(Cabaret::open(&dir)?.current_change()?.to_string())
 }
